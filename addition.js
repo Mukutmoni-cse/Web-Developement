39f80958a26356fd5c707f72618a1ea88c0fb391
Mukutmoni-cse <66082138+Mukutmoni-cse@users.noreply.github.com>
2020-07-14 09:36:14 +0530

    Delete addition.js

--- addition.js
+++
@@ -1,6 +0,0 @@
-function addition(form) {
-    document.getElementById("sum").value = Number(form.x.value) + Number(form.y.value)
-    document.getElementById("subs").value = Number(form.x.value) - Number(form.y.value)
-    document.getElementById("multi").value = Number(form.x.value) * Number(form.y.value)
-    document.getElementById("div").value = Number(form.x.value) / Number(form.y.value)
-}
